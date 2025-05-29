36d999017b1825909216ec8a4fa39624bf1941b5
Md Danish Khan <mddanish@nbc.edu.np>
2025-05-29 13:17:07 +0545

    Update secret_code.py

--- secret_code.py
+++ secret_code.py
@@ -1,8 +1,14 @@
 st = input("enter the word you want: ")
+words = st.split(" ")
 coding = True
 if(coding):
-  if(len(word)>=3):
-    ran1 = "sfs"
-    ran2 = "568"
-    word_new = ran1 + word[1:] + word[0] + ran2
-    print(word_new)
+  nwords = []
+  for word in words:
+    if(len(word)>=3):
+      ran1 = "sfs"
+      ran2 = "568"
+      stnew = ran1 + word[1:] + word[0] + ran2
+      nwords.append(stnew)
+    else:
+      nwords.append(word[::-1])
+  print(" ".join(nwords))
